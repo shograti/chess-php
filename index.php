268d1e0c11ad95191cb2ab51a51b65d6ed019030
shograti <echecsleo@gmail.com>
2022-01-11 13:07:45 +0100

    Added capture function in ChessPiece

--- index.php
+++ index.php
@@ -36,14 +36,33 @@
 
     $whiteKingOnE1->canGoTo(1,1) */
 
-require("Pawn.php");
+/* require("Pawn.php");
 $pawnOnE2 = new Pawn(5,2,1);
 $pawnOnE2->canGoTo(5,4,true);
 $pawnOnE2->canGoTo(5,3,true);
 $pawnOnE2->canGoTo(5,4,false);
 $pawnOnE2->canGoTo(5,5,true);
-    
+     */
+    require('Knight.php');
+    require ('Bishop.php');
+    $darkBishopOnLightSquare = new Bishop(3,3,2);
+    $white_knight_on_b1 = new Knight(2,1,1);
+    var_dump($darkBishopOnLightSquare->getxPosition());
+    var_dump($darkBishopOnLightSquare->getyPosition());
+    var_dump($darkBishopOnLightSquare->getPieceColor());
+    var_dump($white_knight_on_b1->getxPosition());
+    var_dump($white_knight_on_b1->getyPosition());
+    var_dump($white_knight_on_b1->getPieceColor());
 
-    ?>
+/*     if($white_knight_on_b1->getPieceColor()!=$darkBishopOnLightSquare->getPieceColor() && $white_knight_on_b1->canGoTo($darkBishopOnLightSquare->getxPosition(),$darkBishopOnLightSquare->getyPosition()) ){
+        echo("the knight can capture the bishop");
+        return true;
+    } else {
+        echo("the knight can't capture the bishop");
+        return false;
+    }*/
+
+    $white_knight_on_b1->canCapture($white_knight_on_b1,$darkBishopOnLightSquare)
+    ?> 
 </body>
 </html>
